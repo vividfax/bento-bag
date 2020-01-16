8dc7eeb270bd672906fd730b6b47f28791bc8d24
Rianna Suen <vividfax@gmail.com>
2020-01-16 15:29:14 +0000

    Add hand drawn aesthetic

--- diagrams.js
+++ diagrams.js
@@ -1,6 +1,7 @@
 function drawDiagrams() {
 
-    scale(1);
+    noStroke();
+    strokeWeight(1.4);
 
     translate(80, 100);
     drawBag();
@@ -13,54 +14,89 @@ function drawBag() {
 
     const width = 140;
 
-    fill(dark);
-    noStroke();
+    fill(mid);
     triangle(0, 0, 0, width, width, width);
+    scribbleTriangle(0, 0, 0, width, width, width);
 
-    const gap = 2;
-    fill(light);
-    triangle(width - gap, 0, -gap, width, width - gap, width);
-
-    fill(dark);
+    fill(mid);
     triangle(width, 0, 0, width, width, width);
+    scribbleTriangle(width, 0, 0, width, width, width);
 
-    const padding = 15;
-    stroke(dark);
-    strokeWeight(1.4);
-    drawSpan(1, width + padding, width - 1, width + padding);
-    drawSpan(width + padding, 1, width + padding, width - 1);
+    drawSpan(1, width, width - 1, width);
+    drawSpan(width, 1, width, width - 1);
 }
 
 function drawFabric() {
 
-    const width = 305;
-    const height = 105;
+    const seamAllowance = 10;
+    const width = 300 + seamAllowance;
+    const height = 100 + seamAllowance;
 
-    fill(dark);
-    noStroke();
+    fill(mid);
     rect(0, 0, width, height);
+    scribbleRect(0, 0, width, height);
 
-    const padding = 15;
+    drawFolds(width, height, seamAllowance);
+
+    drawSpan(0, height, width, height);
+    drawSpan(width, 0, width, height);
+}
+
+function drawFolds(width, height, pad) {
+
+    const third = (width - pad * 2) / 3;
+
+    stroke(light);
+    drawingContext.setLineDash([3, 5]);
+
+    rect(pad, pad, width - pad * 2, height - pad * 2);
+    line(pad, pad, third + pad, height - pad);
+    line(third + pad, height - pad, third * 2 + pad, pad);
+    line(third * 2 + pad, pad, third * 3 + pad, height - pad);
+
+    drawingContext.setLineDash([]);
+}
+
+function scribbleRect(x, y, width, height) {
+
+    x += (width / 2);
+    y += (height / 2);
+
     stroke(dark);
-    strokeWeight(1.4);
-    drawSpan(1, height + padding, width - 1, height + padding);
-    drawSpan(width + padding, 1, width + padding, height - 1);
+    scribble.scribbleRect(x, y, width, height);
+}
+
+function scribbleTriangle(x1, y1, x2, y2, x3, y3) {
+
+    stroke(dark);
+
+    scribble.scribbleLine(x1, y1, x2, y2);
+    scribble.scribbleLine(x2, y2, x3, y3);
+    scribble.scribbleLine(x3, y3, x1, y1);
 }
 
 function drawSpan(x1, y1, x2, y2) {
 
-    line(x1, y1, x2, y2);
+    const padding = 18;
+    const footWidth = 6;
 
-    const footWidth = 2.5;
+    stroke(mid);
 
     if (x1 == x2) {
 
-        line(x1 - footWidth, y1, x1 + footWidth, y1);
-        line(x2 - footWidth, y2, x2 + footWidth, y2);
+        x1 += padding;
+        x2 += padding;
+
+        scribble.scribbleLine(x1 - footWidth, y1, x1 + footWidth, y1);
+        scribble.scribbleLine(x2 - footWidth, y2, x2 + footWidth, y2);
 
     } else if (y1 == y2) {
 
-        line(x1, y1 - footWidth, x1, y1 + footWidth);
-        line(x2, y2 - footWidth, x2, y2 + footWidth);
+        y1 += padding;
+        y2 += padding;
+
+        scribble.scribbleLine(x1, y1 - footWidth, x1, y1 + footWidth);
+        scribble.scribbleLine(x2, y2 - footWidth, x2, y2 + footWidth);
     }
+    scribble.scribbleLine(x1, y1, x2, y2);
 }
--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ const light = "#f9dbbd";
 const mid = "#ffa5ab";
 const dark = "#a53860";
 
+let scribble = new Scribble();
 let measurements = {};
 
 function setup() {
@@ -11,13 +12,17 @@ function setup() {
 	pixelDensity(2);
 	background(light);
 
+
+	scribble.bowing = 0.1;
+	scribble.roughness = 1.3;
+
 	measurements = {
 
 		bagHeight: new Input(250, 160, 11),
 		bagWidth: new Input(125, 270, 11),
 
-		fabricLength: new Input(635, 260, 8),
-		fabricWidth: new Input(430, 355, 22)
+		fabricLength: new Input(640, 265, 8),
+		fabricWidth: new Input(430, 360, 22)
 	};
 	drawDiagrams();
 }
